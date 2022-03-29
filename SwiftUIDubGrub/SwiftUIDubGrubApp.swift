8f52fba30e78d1dbcc77cf1c872879f1f1c8bb0a
Richard Price <richyprice77@gmail.com>
2022-03-29 09:00:17 +0100

    inital commit

---
+++ SwiftUIDubGrub/SwiftUIDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDubGrubApp.swift
+//  SwiftUIDubGrub
+//
+//  Created by Richard Price on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
