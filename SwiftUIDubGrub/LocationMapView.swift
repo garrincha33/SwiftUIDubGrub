9556812e34ad0f18ca119584dc1a589127b55b9c
Richard Price <richyprice77@gmail.com>
2022-03-29 13:31:22 +0100

    added assets setup tabview for inital setup

---
+++ SwiftUIDubGrub/LocationMapView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationMapView.swift
+//  SwiftUIDubGrub
+//
+//  Created by Richard Price on 29/03/2022.
+//
+
+import SwiftUI
+//step 2 create dummy profile screens
+struct LocationMapView: View {
+    var body: some View {
+        Text("map view")
+    }
+}
+
+struct LocationMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationMapView()
+    }
+}
